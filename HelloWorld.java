f58e1b5a46030fb19c7828156f20e62059974c2f
Bourguignon1264 <70352417+Bourguignon1264@users.noreply.github.com>
2021-06-23 19:08:46 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Trying Wehook Again *****-----");
+		System.out.println("-----***** Trying Webhook 3 *****-----");
 	}
 }
